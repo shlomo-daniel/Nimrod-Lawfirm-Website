9ed321466de6ab9fa0b7f17b579b240e49f1e089
Shlomo Daniel Gindi <shlomodani054@gmail.com>
2025-09-17 06:52:24 +0300

    fix nav uls overflow

--- JS/responsive_nav.js
+++ JS/responsive_nav.js
@@ -1,10 +1,12 @@
 const navContainer = document.querySelector(".nav-container");
-const navList = document.querySelectorAll(".nav-list");
+const navUl = document.querySelectorAll(".nav-list");
 const navIcon = document.getElementById("menu-icon");
-
 navIcon.addEventListener("click", (e) => {
   e.preventDefault();
-  navContainer.classList.toggle("mobile-mode");
   navIcon.classList.toggle("active");
+  navUl.forEach((ul) => {
+    ul.classList.toggle("open-menu");
+  });
+  navContainer.classList.toggle("mobile-mode");
   console.log(navContainer);
 });
